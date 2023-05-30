9b52083c09b55c7541921ed917d95857246cfe93
Rose1845 <odhiamborose466@gmail.com>
2023-05-30 14:17:14 +0300

    africastalking

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        
+        veryDarkBlue:'hsl(233,12%,13%)',
+      
+      }
+    },
+  },
+  plugins: [],
+}
+

f800c12407911587cb3583b6f1ffb7d3dff38204
Rose1845 <odhiamborose466@gmail.com>
2023-05-30 15:43:54 +0300

    building

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // content: ["./src/**/*.{html,js}"],
-  content:["./Src/**/*.{html,js,css}"],
+  content:["./src/**/*.{html,js,css}"],
   theme: {
     extend: {
       colors:{

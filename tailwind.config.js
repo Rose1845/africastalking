c085c3618f174afd1a74edf08db7a387f628fb96
Rose1845 <odhiamborose466@gmail.com>
2023-05-30 15:32:04 +0300

    add report and home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  // content: ["./src/**/*.{html,js}"],
+  content:["../src/**/*.{html,js,css}"],
   theme: {
     extend: {
       colors:{
-        
+        darkGrayishBlue:'hsl(227,12%,61%)',
         veryDarkBlue:'hsl(233,12%,13%)',
       
       }
